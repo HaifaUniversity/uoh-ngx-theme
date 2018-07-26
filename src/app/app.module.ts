b527a2fb9c9ef04eb6796ed89e26cf66712eeb6f
Pablo Saracusti <psaracu1@univ.haifa.ac.il>
2018-07-26 11:48:30 +0300

    chore: Test for accessibility module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { UohAccessibilityModule } from '@uoh/ngx-theme';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, UohAccessibilityModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
