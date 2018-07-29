ffd76f7bc34434ddea59c2e0482cdffad1c24cdb
Pablo Saracusti <psaracu1@univ.haifa.ac.il>
2018-07-29 10:47:31 +0300

    fix: Back to top tag name and test

--- projects/uoh/ngx-theme/src/lib/back-to-top/back-to-top.component.ts
+++ projects/uoh/ngx-theme/src/lib/back-to-top/back-to-top.component.ts
@@ -1,7 +1,7 @@
 import { Component, HostListener } from '@angular/core';
 
 @Component({
-  selector: 'app-back-to-top',
+  selector: 'uoh-back-to-top',
   templateUrl: './back-to-top.component.html',
   styleUrls: ['./back-to-top.component.scss']
 })
@@ -9,7 +9,7 @@ export class BackToTopComponent {
   display = false;
   private BACK_TO_TOP_SCROLL = 100;
 
-  constructor() { }
+  constructor() {}
 
   backToTop(): void {
     window.scrollTo(0, 0);
@@ -19,10 +19,10 @@ export class BackToTopComponent {
   }
 
   @HostListener('window:scroll', ['$event'])
-  private onScroll(event: any): void {
-    const element = (document.documentElement.clientHeight) ? document.documentElement : document.body;
-    const scrollTop = (window.pageYOffset) ? window.pageYOffset : element.scrollTop;
+  onScroll(event: any): void {
+    const element = document.documentElement.clientHeight ? document.documentElement : document.body;
+    const scrollTop = window.pageYOffset ? window.pageYOffset : element.scrollTop;
 
-    this.display = (scrollTop > this.BACK_TO_TOP_SCROLL);
+    this.display = scrollTop > this.BACK_TO_TOP_SCROLL;
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { UohAccessibilityModule, UohSpinnerModule, UohHeaderModule, UohSpinner } from '@uoh/ngx-theme';
+import {
+  UohAccessibilityModule,
+  UohSpinnerModule,
+  UohHeaderModule,
+  UohSpinner,
+  UohBackToTopModule
+} from '@uoh/ngx-theme';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, UohAccessibilityModule, UohSpinnerModule, UohHeaderModule],
+  imports: [BrowserModule, UohAccessibilityModule, UohSpinnerModule, UohHeaderModule, UohBackToTopModule],
   providers: [UohSpinner],
   bootstrap: [AppComponent]
 })
