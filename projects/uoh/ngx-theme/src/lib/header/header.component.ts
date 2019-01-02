5381a240033f5fd1333102e8e70aa3b9400ea383
Pablo Saracusti <psaracu1@univ.haifa.ac.il>
2019-01-02 11:27:23 +0200

    feat(header): Input for logo link & labels object

--- projects/uoh/ngx-theme/src/lib/header/header.component.ts
+++ projects/uoh/ngx-theme/src/lib/header/header.component.ts
@@ -1,5 +1,10 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 
+export interface HeaderLabels {
+  welcome: string;
+  logOut: string;
+}
+
 @Component({
   selector: 'uoh-header',
   templateUrl: './header.component.html',
@@ -10,8 +15,8 @@ export class HeaderComponent {
   @Input() title = 'אוניברסיטת חיפה';
   @Input() subtitle: string;
   @Input() user: string;
-  @Input() welcome = 'שלום';
-  @Input() logout = 'יציאה מהמערכת';
+  @Input() logoLinkUrl = 'https://www.haifa.ac.il/';
+  @Input() labels: HeaderLabels = { welcome: 'שלום', logOut: 'יציאה מהמערכת' };
   @Output() logOut = new EventEmitter<boolean>();
 
   constructor() {}
@@ -21,6 +26,6 @@ export class HeaderComponent {
   }
 
   openHaifaWebsite(): void {
-    window.open('https://www.haifa.ac.il/', '_blank');
+    window.open(this.logoLinkUrl, '_blank');
   }
 }
