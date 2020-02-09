35e01dcb13cf6e31f077779bbee14a03d2fa5ce3
Pablo Saracusti <psaracu1@univ.haifa.ac.il>
2020-02-09 14:08:38 +0200

    fix(schematics): Install material

--- projects/uoh/ngx-theme/schematics/ng-add/install-material.ts
+++ projects/uoh/ngx-theme/schematics/ng-add/install-material.ts
@@ -1,9 +1,21 @@
-import { Rule, SchematicContext, externalSchematic } from '@angular-devkit/schematics';
+import { Rule, SchematicContext, externalSchematic, Tree } from '@angular-devkit/schematics';
+import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
 
 import { Schema } from './schema';
+import { getPackageVersionFromPackageJson, addPackageToPackageJson } from '../utils/package-config';
+
+// The material version will be filled in during compilation from the dependencies in the package.json of this project.
+const materialVersion = `~0.0.0-PLACEHOLDER`;
 
 export function installMaterial(_options: Schema): Rule {
-  return (_, _context: SchematicContext) => {
+  return (host: Tree, _context: SchematicContext) => {
+    const materialInstalled = getPackageVersionFromPackageJson(host, '@angular/material');
+
+    if (!materialInstalled) {
+      addPackageToPackageJson(host, '@angular/material', materialVersion);
+      _context.addTask(new NodePackageInstallTask());
+    }
+
     return externalSchematic('@angular/material', 'ng-add', { ..._options, theme: 'custom' });
   };
 }
---
+++ projects/uoh/ngx-theme/schematics/set-material-version.js
@@ -0,0 +1,49 @@
+const fs = require('fs');
+const path = require('path');
+
+const readJSON = filePath => {
+  const data = fs.readFileSync(filePath, 'utf8');
+  // let content;
+
+  // fs.readFile(filePath, 'utf8', (err, data) => {
+  //   if (err) throw err;
+  //   console.log('file adat', data);
+  //   content = JSON.parse(data);
+  // });
+
+  console.log('data', data);
+
+  return JSON.parse(data);
+};
+
+const readPackageVersion = (configPath, package) => {
+  const config = readJSON(configPath);
+
+  if (config.dependencies && config.dependencies[package]) {
+    return config.dependencies[package];
+  }
+
+  if (config.peerDependencies && config.peerDependencies[package]) {
+    return config.peerDependencies[package];
+  }
+
+  return undefined;
+};
+
+const setSchematicsMaterialVersion = (filePath, version) => {
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) throw err;
+    const content = data.replace('~0.0.0-PLACEHOLDER', version);
+    fs.writeFile(filePath, content, 'utf8', err => {
+      if (err) throw err;
+      console.log(`The material version ${version} was set in the schematics file ${filePath}.`);
+    });
+  });
+};
+
+const setMaterialVersion = (configPath, schematicsFile) => {
+  const version = readPackageVersion(configPath, '@angular/material');
+  setSchematicsMaterialVersion(schematicsFile, version);
+};
+
+setMaterialVersion('package.json', '../../../dist/uoh/ngx-theme/schematics/ng-add/install-material.js');
