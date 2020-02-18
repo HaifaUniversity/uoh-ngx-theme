a866d7f24bd11bf266bfe71fc7f2f00deb1bea79
Pablo Saracusti <psaracu1@univ.haifa.ac.il>
2020-02-18 16:00:32 +0200

    feat(schematics): Get mat versions from ng site

--- projects/uoh/ngx-theme/schematics/ng-add/index.ts
+++ projects/uoh/ngx-theme/schematics/ng-add/index.ts
@@ -4,8 +4,62 @@ import { getPackageVersionFromPackageJson, addPackageToPackageJson } from '../ut
 
 import { Schema } from './schema';
 
-// The material version will be filled in during compilation from the dependencies in the package.json of this project.
-const materialVersion = `~0.0.0-PLACEHOLDER`;
+// The material version will be filled in during compilation from the peer dependencies in the package.json of this project.
+const MAT_VERSION_DEPENDENCY = `~0.0.0-PLACEHOLDER`;
+
+// The material versions will be filled in during the compilation from 'material.angular.io'.
+const MATERIAL_VERSIONS: Array<string> = []; // MAT_VERSIONS_PLACEHOLDER
+
+/**
+ * Retrieves the major version from a version string, i.e.: '^9.0.0' will return '9'.
+ * @param version The version to get the major version from.
+ */
+function getMajorVersion(version: string): string | undefined {
+  try {
+    const matches = version.match(/\d+/gm);
+
+    return !!matches && matches.length > 0 ? matches[0] : undefined;
+  } catch (e) {}
+
+  return undefined;
+}
+
+/**
+ * Retrieves the last material version from the list of MATERIAL_VERSIONS.
+ */
+function getLastMaterialVersion(): string | undefined {
+  try {
+    return `~${MATERIAL_VERSIONS[MATERIAL_VERSIONS.length - 1]}`;
+  } catch (e) {}
+
+  return undefined;
+}
+
+/**
+ * Retrieves the material and cdk version to be installed.
+ * @param host The schematics tree.
+ */
+function getMaterialVersion(host: Tree): string {
+  const ngVersion = getPackageVersionFromPackageJson(host, '@angular/core');
+  const ngMajorVersion = !!ngVersion ? getMajorVersion(ngVersion) : undefined;
+
+  if (MATERIAL_VERSIONS && MATERIAL_VERSIONS.length > 0) {
+    if (ngMajorVersion) {
+      for (const version of MATERIAL_VERSIONS) {
+        if (getMajorVersion(version) === ngMajorVersion) {
+          return `~${version}`;
+        }
+      }
+    }
+
+    const lastMatVersion = getLastMaterialVersion();
+    if (lastMatVersion) {
+      return lastMatVersion;
+    }
+  }
+
+  return MAT_VERSION_DEPENDENCY;
+}
 
 /**
  * Angular ngAdd schematics that adds all the uoh-theme configurations.
@@ -15,6 +69,7 @@ export function ngAdd(options: Schema): Rule {
   return (host: Tree, context: SchematicContext) => {
     const materialInstalled = getPackageVersionFromPackageJson(host, '@angular/material');
     const cdkInstalled = getPackageVersionFromPackageJson(host, '@angular/cdk');
+    const materialVersion = getMaterialVersion(host);
 
     if (!materialInstalled) {
       addPackageToPackageJson(host, '@angular/material', materialVersion);
--- projects/uoh/ngx-theme/schematics/set-material-version.js
+++ projects/uoh/ngx-theme/schematics/set-material-version.js
@@ -1,4 +1,6 @@
 const fs = require('fs');
+const chalk = require('chalk');
+const request = require('request');
 
 const readJSON = filePath => {
   const data = fs.readFileSync(filePath, 'utf8');
@@ -21,12 +23,31 @@ const readPackageVersion = (configPath, package) => {
 };
 
 const setSchematicsMaterialVersion = (filePath, version) => {
+  // Set the fallback material version: the version used as peer dependency in this project.
   fs.readFile(filePath, 'utf8', (err, data) => {
     if (err) throw err;
     const content = data.replace('~0.0.0-PLACEHOLDER', version);
     fs.writeFile(filePath, content, 'utf8', err => {
       if (err) throw err;
-      console.log(`The material version ${version} was set in the schematics file ${filePath}.`);
+      console.log(chalk.yellow.bold(`The material version ${version} was set in the schematics file ${filePath}.`));
+    });
+  });
+
+  // Set the list of material versions from material.angular.io.
+  request('https://material.angular.io/assets/versions.json', (error, response, body) => {
+    if (error && response.statusCode !== 200) throw error;
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) throw err;
+      const results = JSON.parse(body);
+      const versionsArray = results.map(item => item.title);
+      const versions = JSON.stringify(versionsArray);
+      const content = data.replace(`[]; // MAT_VERSIONS_PLACEHOLDER`, `${versions};`);
+      fs.writeFile(filePath, content, 'utf8', err => {
+        if (err) throw err;
+        console.log(
+          chalk.yellow.bold(`The list of material versions ${versions} was set in the schematics file ${filePath}.`)
+        );
+      });
     });
   });
 };
