3eca34c71722d77562ef0c03ea890be313bd3257
Pablo Saracusti <psaracu1@univ.haifa.ac.il>
2020-09-23 09:28:58 +0300

    fix(footer): Remove package.json import

--- projects/uoh/ngx-theme/src/lib/footer/footer.component.ts
+++ projects/uoh/ngx-theme/src/lib/footer/footer.component.ts
@@ -1,7 +1,5 @@
 import { Component, Input, OnInit, HostBinding } from '@angular/core';
 
-// import * as app from 'package.json';
-
 @Component({
   selector: 'uoh-footer',
   templateUrl: './footer.component.html',
@@ -13,24 +11,5 @@ export class FooterComponent implements OnInit {
 
   constructor() {}
 
-  ngOnInit(): void {
-    if (this.version === undefined) {
-      this.version = this.getVersion();
-    }
-  }
-
-  /**
-   * Retrieves the version automatically from the package.json file of the project.
-   */
-  private getVersion(): string {
-    // Try to automatically retrieve the version number
-    try {
-      // FIXME: Use Ecmascript import to import the package.json. Add schematics to import the version in the app itself.
-      // return app.version;
-    } catch (e) {
-      console.warn('Cannot automatically retrieve the app version', e);
-    }
-
-    return undefined;
-  }
+  ngOnInit(): void {}
 }
